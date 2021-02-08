7c0d626f2d389277cd25c607b32f83fae7f573a1
Miriam <miriam.haenle@gmail.com>
2021-02-08 18:04:33 +0100

    Added array exercise

---
+++ src/exercises/01_filter_sort_dom.js
@@ -0,0 +1,49 @@
+// More exercices about array methods
+console.clear();
+
+const hogwartsStudents = [
+  {
+    name: 'Harry',
+    lastName: 'Potter',
+    house: { name: 'Gryffindor', color: 'red' },
+  },
+  {
+    name: 'Ron',
+    lastName: 'Weasley',
+    house: { name: 'Gryffindor', color: 'red' },
+  },
+  {
+    name: 'Hermione',
+    lastName: 'Granger',
+    house: { name: 'Gryffindor', color: 'red' },
+  },
+  {
+    name: 'Draco',
+    lastName: 'Malfoy',
+    house: { name: 'Slytherin', color: 'green' },
+  },
+  {
+    name: 'Cedric',
+    lastName: 'Diggory',
+    house: { name: 'Hufflepuff', color: 'yellow' },
+  },
+  {
+    name: 'Luna',
+    lastName: 'Lovegood',
+    house: { name: 'Ravenclaw', color: 'blue' },
+  },
+];
+
+// 1. Create a container for our cards with a class name 'card-container'
+
+// 2. Create a card for each student, with a class name 'card'
+// 2.1 The background color of the card should be the corresponding house color
+// 2.2 The name, lastname and house should be displayed in the card (hint: innerHTML)
+
+// 3. Create a button, on a click the students should be sorted alphabetically
+
+// 4. Create a button, on a click only gryffindor students should appear
+
+// After you are done with this we should clean up the code a bit!
+// Here is a link to a cleaned up version of this pen:
+// https://codepen.io/macfrei/pen/GRNoKNG
