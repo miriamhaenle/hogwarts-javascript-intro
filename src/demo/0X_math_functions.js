9e09a6852f2eaf3f9ee238a61be25ffe1cd2eaef
Miriam <miriam.haenle@gmail.com>
2021-02-08 18:02:02 +0100

    Added weasleys

--- src/demo/0X_math_functions.js
+++ src/demo/0X_math_functions.js
@@ -3,3 +3,54 @@ let someNumber = 4.48;
 console.log(Math.round(someNumber));
 console.log(Math.ceil(someNumber));
 console.log(Math.floor(someNumber));
+
+// Array of objects
+const spells = [
+  {
+    spell: 'Alohomora',
+    description: 'Unlocking charm',
+  },
+  {
+    spell: 'Bombarda',
+    description: 'Exploding charm',
+  },
+  {
+    spell: 'Engorgio',
+    description: 'Engorgement charm',
+  },
+  {
+    spell: 'Expecto Patronum',
+    description: 'Patronus charm',
+  },
+  {
+    spell: 'Expelliarmus',
+    description: 'Disarming charm',
+  },
+  {
+    spell: 'Finite Incantatem',
+    description: 'General counter-spell',
+  },
+  {
+    spell: 'Immobulus',
+    description: 'Freezing charm',
+  },
+  {
+    spell: 'Incendio',
+    description: 'Fire-making spell',
+  },
+  {
+    spell: 'Lumos',
+    description: 'Wand-lighting charm',
+  },
+  {
+    spell: 'Petrificus Totalus',
+    description: 'Full Body-Bind Curse',
+  },
+];
+
+function castSpell(spells) {
+  const index = Math.floor(Math.random() * spells.length);
+  return `Harry casts: ${spells[index].spell.toUpperCase()}!`;
+}
+
+console.log(castSpell(spells));
