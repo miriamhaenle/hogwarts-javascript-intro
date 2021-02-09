d8ed9047314cd91662accb65567d3dfb3775f6c8
Miriam <miriam.haenle@gmail.com>
2021-02-09 16:31:28 +0100

    Refactor solution

--- src/exercises/02_sort_filter_dom_cards.js
+++ src/exercises/02_sort_filter_dom_cards.js
@@ -34,47 +34,108 @@ const hogwartsStudents = [
 ];
 
 // 1. Create a container for our cards with a class name 'card-container'
-const studentCardContainer = document.createElement('div');
-studentCardContainer.className = 'card-container';
+function createCardContainer() {
+  const studentCardContainer = document.createElement('div');
+  studentCardContainer.classList.add('card-container');
+  return studentCardContainer;
+}
+const studentCardContainer = createCardContainer();
 document.body.append(studentCardContainer);
-createStudentCards(hogwartsStudents);
+
+const studentCards = createStudentCards(hogwartsStudents);
+studentCardContainer.append(...studentCards);
 
 // 2. Create a card for each student, with a class name 'card'
 // 2.1 The background color of the card should be the corresponding house color
 // 2.2 The name, lastname and house should be displayed in the card (hint: innerHTML)
 function createStudentCards(students) {
-  students.forEach((student) => {
+  return students.map((student) => {
     const studentCard = document.createElement('div');
-    studentCard.className = 'card';
+    studentCard.classList.add('card');
     studentCard.style.background = student.house.color;
-    studentCard.innerHTML = `
-   <h2>${student.name} ${student.lastName}</h2>
-   <span>House: ${student.house.name}</span>
-  `;
-    studentCardContainer.append(studentCard);
+    studentCard.innerHTML = studentCardContent(student);
+    return studentCard;
   });
 }
 
+function studentCardContent(student) {
+  return `<h2>${student.name} ${student.lastName}</h2>
+  <span>House: ${student.house.name}</span>
+ `;
+}
 // 3. Create a button, on a click the students should be sorted alphabetically
-const sortButton = document.createElement('button');
-sortButton.textContent = 'Sort!';
-sortButton.addEventListener('click', () => {
-  hogwartsStudents.sort((a, b) => (a.name > b.name ? 1 : -1));
-  studentCardContainer.innerHTML = '';
-  createStudentCards(hogwartsStudents);
-});
+const sortButton = createButton('Sort!');
+
+sortButton.addEventListener('click', () => sortStudentCards(hogwartsStudents));
+
+function sortStudentCards(students) {
+  const sortedStudents = sortStudents(students);
+  const sortedStudentCards = createStudentCards(sortedStudents);
+  setContent(sortedStudentCards, cardContainer());
+}
+
+function sortStudents(students) {
+  return students.sort((a, b) => (a.name > b.name ? 1 : -1));
+}
+
 document.body.append(sortButton);
 
 // 4. Create a button, on a click only gryffindor students should appear
-const filterButton = document.createElement('button');
-filterButton.textContent = 'Filter!';
+const filterButton = createButton('Filter!');
+
 filterButton.addEventListener('click', () => {
-  const filteredStudents = hogwartsStudents.filter(
-    (student) => student.house.name === 'Gryffindor'
-  );
-  studentCardContainer.innerHTML = '';
-  createStudentCards(filteredStudents);
+  const filteredStudentCards = filterStudentCards(hogwartsStudents);
+  setContent(filteredStudentCards, cardContainer());
 });
+
+function setContent(elements, container = document.body) {
+  container.innerHTML = '';
+  container.append(...elements);
+}
+
+function filterStudentCards(students) {
+  const filteredStudents = filterStudents(students);
+  return createStudentCards(filteredStudents);
+}
+
+function filterStudents(students) {
+  return students.filter((student) => student.house.name === 'Gryffindor');
+}
+
 document.body.append(filterButton);
 
 // After you are done with this we should clean up the code a bit!
+// Some useful helper functions
+
+function createButton(text = 'Button') {
+  const button = document.createElement('button');
+  button.textContent = text;
+  return button;
+}
+
+function cardContainer() {
+  return document.querySelector('.card-container');
+}
+
+// Add a randomizer button
+const randomButton = createButton('Randomize!');
+document.body.append(randomButton);
+
+randomButton.addEventListener('click', () => {
+  const randomizedStudents = randomizeStudentOrder(hogwartsStudents);
+  const randomizedStudentCards = createStudentCards(randomizedStudents);
+  setContent(randomizedStudentCards, cardContainer());
+});
+
+// See the following link for explanation about randomizer function for arrays: https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+
+function randomizeStudentOrder(students) {
+  for (let i = students.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const temp = students[i];
+    students[i] = students[j];
+    students[j] = temp;
+  }
+  console.log(students);
+  return students;
+}
