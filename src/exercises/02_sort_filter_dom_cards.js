a7cd6c2a243c8c6d730c62d1b7aa30397e4188d3
Miriam <miriam.haenle@gmail.com>
2021-02-09 11:40:41 +0100

    Added solution for cards exercise

--- src/exercises/02_sort_filter_dom_cards.js
+++ src/exercises/02_sort_filter_dom_cards.js
@@ -1,4 +1,5 @@
 // More exercices about array methods
+
 const hogwartsStudents = [
   {
     name: 'Harry',
@@ -33,13 +34,47 @@ const hogwartsStudents = [
 ];
 
 // 1. Create a container for our cards with a class name 'card-container'
+const studentCardContainer = document.createElement('div');
+studentCardContainer.className = 'card-container';
+document.body.append(studentCardContainer);
+createStudentCards(hogwartsStudents);
 
 // 2. Create a card for each student, with a class name 'card'
 // 2.1 The background color of the card should be the corresponding house color
 // 2.2 The name, lastname and house should be displayed in the card (hint: innerHTML)
+function createStudentCards(students) {
+  students.forEach((student) => {
+    const studentCard = document.createElement('div');
+    studentCard.className = 'card';
+    studentCard.style.background = student.house.color;
+    studentCard.innerHTML = `
+   <h2>${student.name} ${student.lastName}</h2>
+   <span>House: ${student.house.name}</span>
+  `;
+    studentCardContainer.append(studentCard);
+  });
+}
 
 // 3. Create a button, on a click the students should be sorted alphabetically
+const sortButton = document.createElement('button');
+sortButton.textContent = 'Sort!';
+sortButton.addEventListener('click', () => {
+  hogwartsStudents.sort((a, b) => (a.name > b.name ? 1 : -1));
+  studentCardContainer.innerHTML = '';
+  createStudentCards(hogwartsStudents);
+});
+document.body.append(sortButton);
 
 // 4. Create a button, on a click only gryffindor students should appear
+const filterButton = document.createElement('button');
+filterButton.textContent = 'Filter!';
+filterButton.addEventListener('click', () => {
+  const filteredStudents = hogwartsStudents.filter(
+    (student) => student.house.name === 'Gryffindor'
+  );
+  studentCardContainer.innerHTML = '';
+  createStudentCards(filteredStudents);
+});
+document.body.append(filterButton);
 
 // After you are done with this we should clean up the code a bit!
