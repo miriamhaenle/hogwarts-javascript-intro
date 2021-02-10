371f4ed056b0f30435bce1d04923534817189898
Miriam <miriam.haenle@gmail.com>
2021-02-10 16:01:40 +0100

    Added demo

--- src/demo/08_local_storage.js
+++ src/demo/08_local_storage.js
@@ -1,1 +1,30 @@
-console.log('local Storage');
+const form = document.querySelector('form');
+const inputField = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+let shoppingList = JSON.parse(localStorage.getItem('WizardingEquipment')) || [];
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  shoppingList.push(inputField.value);
+  console.log(shoppingList);
+  localStorage.setItem('WizardingEquipment', JSON.stringify(shoppingList));
+
+  const listItem = renderListeItem(inputField.value);
+  list.appendChild(listItem);
+
+  inputField.value = '';
+});
+
+shoppingList.forEach((listItem) => {
+  const item = renderListeItem(listItem);
+  list.appendChild(item);
+});
+
+function renderListeItem(item) {
+  const el = document.createElement('li');
+  el.textContent = item;
+  return el;
+}
