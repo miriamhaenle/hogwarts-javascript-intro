918d429a4755d82a0e8db54b788c5e25260572b0
Miriam <miriam.haenle@gmail.com>
2021-02-10 07:28:11 +0100

    Refactor local storage implementation

--- src/demo/08_local_storage.js
+++ src/demo/08_local_storage.js
@@ -3,21 +3,33 @@ const inputField = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
-let shoppinglist =
-  JSON.parse(localStorage.getItem('Wizarding equipment')) || [];
+let shoppinglist = loadFromLocal('Wizarding equipment') || [];
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  liMaker(inputField.value);
-  shoppinglist.push(inputField.value);
-  localStorage.setItem('Wizarding equipment', JSON.stringify(shoppinglist));
-  inputField.value = '';
+
+  addItemToList(inputField.value);
+  renderList(inputField.value);
+  saveToLocal('Wizarding equipment', shoppinglist);
+  form.reset();
 });
 
-const liMaker = (text) => {
+shoppinglist.forEach((listItem) => renderList(listItem));
+
+function addItemToList(item) {
+  shoppinglist = [...shoppinglist, item];
+}
+
+function renderList(text) {
   const li = document.createElement('li');
   li.textContent = text;
   list.appendChild(li);
-};
+}
 
-shoppinglist.forEach((listItem) => liMaker(listItem));
+function saveToLocal(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadFromLocal(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
